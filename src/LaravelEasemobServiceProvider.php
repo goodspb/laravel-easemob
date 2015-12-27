7da12a34b8c558f46846aefcad187f9c1ab28956
Aobo <aobozhang@gmail.com>
2015-12-27 23:30:53 +0800

    Use Whole Office Realse Class with full function
    Use Origin SDK From Official Release v1.0.0 (with little bug fix).
    drop Configuration  in Facades。

--- src/LaravelEasemobServiceProvider.php
+++ src/LaravelEasemobServiceProvider.php
@@ -4,46 +4,30 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelEasemobServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+    * Bootstrap the application services.
+    *
+    * @return void
+    */
+    public function boot()
+    {
+
+        // $this->mergeConfigFrom(__DIR__.'/config/easemob.php', 'easemob');
+    }
 
     /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
+    * Register the service provider.
+    *
+    * @return void
+    */
+    public function register()
     {
         $this->publishes([
             __DIR__.'/config/easemob.php' => config_path('easemob.php'),
         ]);
-        $this->mergeConfigFrom(__DIR__.'/config/easemob.php', 'easemob');
-    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        $this->app->bind('easemob', function ($app) {
-            return new easemob($app);
+        $this->app->singleton('easemobClass', function($app){
+            return new EasemobClass($app['config']['easemob']);
         });
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
-	}
-
+    }
 }
--- src/config/easemob.php
+++ src/config/easemob.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-    'EASEMOB_DOMAIN' => env('easemob_domain', ''),          //域名
-    'ORG_NAME'       => env('easemob_org_name', ''),        //公司名称
-    'APP_NAME'       => env('easemob_app_name', ''),        //应用名称
-    'CLIENT_ID'       => env('easemob_client_id', ''),
-    'CLIENT_SECRET'   => env('easemob_client_secret', ''),
-    'TOKEN_PATH'     => env('token_path','easemob.token'),               //token储存的位置，默认不用修改
+    'org_name'      => env('EASEMOB_ORG_NAME', ''),
+    'app_name'      => env('EASEMOB_APP_NAME', ''),
+    'client_id'     => env('EASEMOB_CLIENT_ID', ''),
+    'client_secret' => env('EASEMOB_CLIENT_SECRET', ''),
 ];
